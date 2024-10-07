5a2c8b6a55aab9243e1764707c4ef5ca85739779
felipe <enggazolla@gmail.com>
2024-10-07 10:53:45 -0300

    theme configs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,29 @@
-import { Text, View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import {
   useFonts,
   Karla_400Regular,
   Karla_700Bold,
 } from '@expo-google-fonts/karla'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+import { config } from './config/gluestack-ui.config'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bg="$redDefault">
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <View />
+      )}
+    </GluestackUIProvider>
   )
 }
